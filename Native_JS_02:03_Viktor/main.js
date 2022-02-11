6c16b7f85c52abae6e5ce60eb5a23cb52d5e526b
Irina Novakovich <irinaredhood@gmail.com>
2022-02-11 23:02:20 +0300

    sord student according their scores

--- Native_JS_02:03_Viktor/main.js
+++ Native_JS_02:03_Viktor/main.js
@@ -15,3 +15,36 @@ const compare = (a , b) => {
 const compare1 = (a , b) => a - b;
 
 console.log(numbers.sort(compare).reverse())
+
+
+const students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    }
+]
+
+//отсортировать студенов по успеваемости
+console.log(students.sort((a, b) => b.scores - a.scores))
+console.log(students.sort((a, b) => b.scores - a.scores))
+
