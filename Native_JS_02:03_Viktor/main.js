471e803919e999d8285c016b95213e9eb3b3d5c1
Irina Novakovich <irinaredhood@gmail.com>
2022-02-11 20:17:04 +0300

    sort numbers

--- Native_JS_02:03_Viktor/main.js
+++ Native_JS_02:03_Viktor/main.js
@@ -1,3 +1,7 @@
-const names = ['Bob', 'Alex', 'Donald', 'John']
+const names = ['Bob', 'Alex', 'Donald', 'John', 'ann', 100, 5]
 
 console.log(names.sort())
+
+const numbers = [100, 3333, 3, 9, 42, 1010]
+
+console.log(numbers.sort())
